02b7c1659b20279ba26a06a23cfce63d2f7ea294
EslamKholeef <eslam.kholeef@gmail.com>
2023-04-19 01:52:50 +0200

    Changing in Landing Page

--- src/app/user/my-cars/my-cars.component.ts
+++ src/app/user/my-cars/my-cars.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserServicesService } from '../Services/user-services.service';
+import { ICar } from 'src/app/Models/ICar';
 
 
 @Component({
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-cars.component.scss']
 })
 export class MyCarsComponent {
+  NID = String(sessionStorage.getItem('userNID'));
+  Cars:ICar[]=[];
+  constructor(private service:UserServicesService) {}
+
+  // ngOnInit(): void {
+  //   this.service.getAllCars()
+  //   .subscribe({
+  //     next : (Requests) => {
+  //       this.Cars = cars.filter(c => c.ownerId==this.NID);
+  //       console.log("Iam Here! in this Car Service For Owner");
+  //     },
+  //     error : (response)=> {console.log(response)}
+  //   });
+  // }
 
 }
