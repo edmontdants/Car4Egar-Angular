606cd37aed0bc41db9b54b3d4308b0f9b7c37057
[Massoud Ezat] <[massoudezat@gmail.com]>
2023-04-19 11:02:20 +0200

    Pay

--- src/app/user/my-borrowings/my-borrowings.component.ts
+++ src/app/user/my-borrowings/my-borrowings.component.ts
@@ -86,5 +86,48 @@ export class MyBorrowingsComponent implements AfterViewInit {
 
     this.service.DeleteCarRequest(vin).subscribe(observer)
   }
+  // pay(amount: any) {
+
+  //   var handler = (<any>window).StripeCheckout.configure({
+  //     key: 'pk_test_51MxpbiGrFuhrPlyrDhgxHm4JMskNCS9Xqqk2lg4niIEFiGoNndiScowyR83RrrTNNJ8E06faO52ybZAC3FemEIxk005hz8Ns0m',
+  //     locale: 'auto',
+  //     token: function (token: any) {
+  //       // You can access the token ID with `token.id`.
+  //       // Get the token ID to your server-side code for use.
+  //       console.log(token)
+  //       alert('Payment Success !!');
+  //     }
+  //   });
+
+  //   handler.open({
+  //     name: 'Car 4 Egar Payment Card',
+  //     description: 'all field are required',
+  //     amount: amount
+  //   });
+
+  // }
+
+  // loadStripe() {
+
+  //   if(!window.document.getElementById('stripe-script')) {
+  //     var s = window.document.createElement("script");
+  //     s.id = "stripe-script";
+  //     s.type = "text/javascript";
+  //     s.src = "https://checkout.stripe.com/checkout.js";
+  //     s.onload = () => {
+  //       this.handler = (<any>window).StripeCheckout.configure({
+  //         key: 'pk_test_51MxpbiGrFuhrPlyrDhgxHm4JMskNCS9Xqqk2lg4niIEFiGoNndiScowyR83RrrTNNJ8E06faO52ybZAC3FemEIxk005hz8Ns0m',
+  //         locale: 'auto',
+  //         token: function (token: any) {
+  //           // You can access the token ID with `token.id`.
+  //           // Get the token ID to your server-side code for use.
+  //           console.log(token)
+  //           alert('Payment Success!!');
+  //         }
+  //       });
+  //     }
+
+  //     window.document.body.appendChild(s);
+  //   }
 
 }
