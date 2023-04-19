0ef612ddb7216c65c1b944e48adb41fbf6e77752
EslamKholeef <eslam.kholeef@gmail.com>
2023-04-19 11:45:15 +0200

    pp

--- src/app/user/my-borrowings/my-borrowings.component.ts
+++ src/app/user/my-borrowings/my-borrowings.component.ts
@@ -16,6 +16,59 @@ export class MyBorrowingsComponent implements AfterViewInit {
    *
    */
   constructor(private service:UserServicesService,private _snackBar: MatSnackBar) {}
+
+
+  stripeAPIKey: any = 'pk_test_51MxpbiGrFuhrPlyrDhgxHm4JMskNCS9Xqqk2lg4niIEFiGoNndiScowyR83RrrTNNJ8E06faO52ybZAC3FemEIxk005hz8Ns0m';
+  handler:any = null;
+
+
+  pay(amount: any) {
+
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51MxpbiGrFuhrPlyrDhgxHm4JMskNCS9Xqqk2lg4niIEFiGoNndiScowyR83RrrTNNJ8E06faO52ybZAC3FemEIxk005hz8Ns0m',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log(token)
+        alert('Payment Success !!');
+      }
+    });
+
+    handler.open({
+      name: 'Car 4 Egar Payment Card',
+      description: 'all field are required',
+      amount: amount
+    });
+
+  }
+
+  loadStripe() {
+
+    if(!window.document.getElementById('stripe-script')) {
+      var s = window.document.createElement("script");
+      s.id = "stripe-script";
+      s.type = "text/javascript";
+      s.src = "https://checkout.stripe.com/checkout.js";
+      s.onload = () => {
+        this.handler = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_51MxpbiGrFuhrPlyrDhgxHm4JMskNCS9Xqqk2lg4niIEFiGoNndiScowyR83RrrTNNJ8E06faO52ybZAC3FemEIxk005hz8Ns0m',
+          locale: 'auto',
+          token: function (token: any) {
+            // You can access the token ID with `token.id`.
+            // Get the token ID to your server-side code for use.
+            console.log(token)
+            alert('Payment Success!!');
+          }
+        });
+      }
+
+      window.document.body.appendChild(s);
+    }
+  }
+
+
+
   ngAfterViewInit(): void {
     throw new Error('Method not implemented.');
   }
@@ -32,7 +85,7 @@ export class MyBorrowingsComponent implements AfterViewInit {
       },
       error : (response)=> {console.log(response)}
     });
-
+    this.loadStripe();
   }
 
   Acceptedreq(vin:any,acc :number){
